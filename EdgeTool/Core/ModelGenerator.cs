5443db20b5c3a95339ecb19d985d7a4ffca1d7de
Mygod <mygodstudio@gmail.com>
2014-05-17 13:58:06 +0800

    EdgeTool 9.1.47.250 (Model bugfix)

--- EdgeTool/Core/ModelGenerator.cs
+++ EdgeTool/Core/ModelGenerator.cs
@@ -17,8 +17,8 @@ namespace Mygod.Edge.Tool
             ZNormals = new[] { z, z, z, z, z, z };
         }
         private const string ModelsNamespace = "050DB82A";
-        private static readonly string[] ChildModels = { "073A8F44", "1DE2AE87", "8A7DBFAE", "451F8839" },
-                                         Materials = { "4F34C26E", "55ECE3AD", "C273F284", "0D11C513" };
+        private static readonly string[] ChildModels = { "67228D77", "1DE2AE87", "8A7DBFAE", "451F8839" },
+                                         Materials = { "2F2CC05D", "55ECE3AD", "C273F284", "0D11C513" };
         private static readonly Vec3[] Translates = { new Vec3(102.5F, 2.25F, -54.5F), new Vec3(89.5F, 2.25F, -90),
                                                       new Vec3(70.5F, 2.25F, -22.5F), new Vec3(30, 2.25F, -74.5F) };
         private static readonly Vec3[] XNormals, YNormals, ZNormals;    // normals for two triangles
